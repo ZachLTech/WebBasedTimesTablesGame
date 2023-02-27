350b6b23f0cb1e45f99809b2758c04c1eef108da
ZachLTech <zlopezzl2021@gmail.com>
2023-02-27 13:05:18 -0500

    modified:   assets/scripts/main.py 	new file:   t3.html

--- assets/scripts/main.py
+++ assets/scripts/main.py
@@ -1,17 +1,18 @@
 from gameclass import Game
 
-Setup = input("\n\nWelcome!\nDo you want to play (1) timed? or (2) Not timed?\n\n > ")
-while(True):
-    if(Setup == "1" or Setup == "timed"):
-        Time = input("\n\nWhat do you want the time limit to be (in seconds)?\n\n > ")
-        Start = Game.Timed(Time)
-        if(Start == -1):
-            print("\n\nGoodbye.\n\n")
-            break
-    if(Setup == "2" or Setup == "not timed"):
-        Start = Game.NoTime()
-        if(Start == -1):
-            print("\n\nGoodbye.\n\n")
-            break
-    else:
-        Setup = input("That is not an option. \nDo you want to play (1) timed? or (2) Not timed?\n\n > ")
+def Start():
+    Setup = input("\n\nWelcome!\nDo you want to play (1) timed? or (2) Not timed?\n\n > ")
+    while(True):
+        if(Setup == "1" or Setup == "timed"):
+            Time = input("\n\nWhat do you want the time limit to be (in seconds)?\n\n > ")
+            Start = Game.Timed(Time)
+            if(Start == -1):
+                print("\n\nGoodbye.\n\n")
+                break
+        if(Setup == "2" or Setup == "not timed"):
+            Start = Game.NoTime()
+            if(Start == -1):
+                print("\n\nGoodbye.\n\n")
+                break
+        else:
+            Setup = input("That is not an option. \nDo you want to play (1) timed? or (2) Not timed?\n\n > ")
